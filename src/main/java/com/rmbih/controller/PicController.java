6b6600405ae0cae82fdf438d37170935020d31f4
WangRex <503431920@qq.com>
2016-06-06 14:29:23 +0800

    Add http method type.

--- src/main/java/com/rmbih/controller/PicController.java
+++ src/main/java/com/rmbih/controller/PicController.java
@@ -14,6 +14,7 @@ import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.rmbih.dao.RmbihPicTMapper;
@@ -34,7 +35,7 @@ public class PicController {
 	@Resource
 	private RmbihPicTMapper rmbihPicTMapper;
 	
-	@RequestMapping("/getPicList")
+	@RequestMapping(value="/getPicList", method=RequestMethod.GET)
 	@ResponseBody
 	public List<RmbihPicT> getPicList(int userId) {
 		RmbihPicTExample rmbihPicTExample = new RmbihPicTExample();
